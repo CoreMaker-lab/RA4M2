cf0aeb88ffcf01f44850e2172de0c9140e9897e2
CoreMaker <a845656974@outlook.com>
2025-03-04 00:30:53 +0800

    update

---
+++ RA4M2_Project20/Debug/ra/fsp/src/r_dtc/subdir.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../ra/fsp/src/r_dtc/r_dtc.c 
+
+C_DEPS += \
+./ra/fsp/src/r_dtc/r_dtc.d 
+
+OBJS += \
+./ra/fsp/src/r_dtc/r_dtc.o 
+
+SREC += \
+RA4M2_Project20.srec 
+
+MAP += \
+RA4M2_Project20.map 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+ra/fsp/src/r_dtc/%.o: ../ra/fsp/src/r_dtc/%.c
+	$(file > $@.in,-mcpu=cortex-m33 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16 -O2 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-strict-aliasing -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal -g -D_RENESAS_RA_ -D_RA_CORE=CM33 -D_RA_ORDINAL=1 -DQE_TOUCH_CONFIGURATION -I"C:/Users/a8456/e2_studio/workspace_5.8/RA4M2_Project20/src" -I"." -I"C:/Users/a8456/e2_studio/workspace_5.8/RA4M2_Project20/ra/fsp/inc" -I"C:/Users/a8456/e2_studio/workspace_5.8/RA4M2_Project20/ra/fsp/inc/api" -I"C:/Users/a8456/e2_studio/workspace_5.8/RA4M2_Project20/ra/fsp/inc/instances" -I"C:/Users/a8456/e2_studio/workspace_5.8/RA4M2_Project20/ra/arm/CMSIS_6/CMSIS/Core/Include" -I"C:/Users/a8456/e2_studio/workspace_5.8/RA4M2_Project20/ra_gen" -I"C:/Users/a8456/e2_studio/workspace_5.8/RA4M2_Project20/ra_cfg/fsp_cfg/bsp" -I"C:/Users/a8456/e2_studio/workspace_5.8/RA4M2_Project20/ra_cfg/fsp_cfg" -I"C:/Users/a8456/e2_studio/workspace_5.8/RA4M2_Project20/qe_gen" -std=c99 -Wno-stringop-overflow -Wno-format-truncation --param=min-pagesize=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" -x c "$<")
+	@echo Building file: $< && arm-none-eabi-gcc @"$@.in"
+
